d973d099e2d20f2aac48cc93133c7dbdb5edcbb2
petit-pierre <aubree.pierre2@gmail.com>
2025-04-15 18:22:51 +0200

    V2.4.1 refonte accueil en cours

--- src/components/Accueil/index.jsx
+++ src/components/Accueil/index.jsx
@@ -3,12 +3,37 @@ import Button from "../Button";
 import "./accueil.css";
 import { useDispatch, useSelector } from "react-redux";
 import { userSlice } from "../../Slices/userSlice";
+import { useEffect, useState } from "react";
 
 function Accueil() {
-  const dispatch = useDispatch();
+  //const dispatch = useDispatch();
   const language = useSelector((state) => state.data.language);
   const translations = useSelector((state) => state.data.translations);
-  const discuss = useSelector((state) => state.data.contactMenu);
+  //const discuss = useSelector((state) => state.data.contactMenu);
+
+  const [scrolling, setScrolling] = useState(false);
+  const [scrollToTop, setScrollToTop] = useState(0);
+
+  useEffect(() => {
+    const onScroll = (e) => {
+      setScrollToTop(e.target.documentElement.scrollTop);
+      setScrolling(e.target.documentElement.scrollTop > scrollToTop);
+    };
+    window.addEventListener("scroll", onScroll);
+    document.querySelector(".oblique").style.width = `calc( 33% + ${
+      scrollToTop / 5
+    }%)`;
+    document.querySelector(".oblique").style.left = `calc( 33% - ${
+      scrollToTop / 10
+    }%)`;
+    document.querySelector(".portrait").style.left = `calc( 2.5% - ${
+      scrollToTop / 20
+    }%)`;
+    /*document.querySelector(".hardSkills").style.margin = `calc( 2.5% - ${
+      scrollToTop / 20
+    }%)`;*/
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollToTop]);
 
   let Hscreen = window.innerHeight;
   if (Hscreen < 650) {
@@ -21,6 +46,14 @@ function Accueil() {
   }
   return (
     <div className="accueilField">
+      <div className="picturePlace">
+        <img
+          src="https://pierre-le-developpeur.com/assets/photo.jpg"
+          alt="portrait"
+          className="portrait"
+        ></img>
+        <div className="oblique"></div>
+      </div>
       <div className="intro">
         {language === "FR" ? <h1>Développeur web</h1> : <h1>Web developer</h1>}
       </div>
@@ -95,14 +128,39 @@ function Accueil() {
             <h3>React.js,</h3>
           </div>
           <div className="oneSkill">
-            <h3>Node.js</h3>
+            <h3>Node.js et</h3>
           </div>
           <div className="oneSkill">
             {language === "FR" ? (
-              <h3>quenouilles quantiques</h3>
+              <h3>autres quenouilles quantiques</h3>
             ) : (
               <h3>quantum distaffs</h3>
             )}
+          </div>
+          <div className="texte_intro">
+            <p>
+              Je suis développeur web front-end, passionné par la création de
+              solutions digitales innovantes et sur mesure.
+            </p>
+            <p>
+              Mon expertise en React, combinée à mes compétences en design et
+              développement back-end, me permet de concevoir des expériences
+              utilisateur performantes et originales.
+            </p>
+            <p>
+              Mon approche est axée sur les bonnes pratiques du web, telles que
+              l’accessibilité, le green IT et le SEO, garantissant ainsi des
+              projets durables et optimisés.
+            </p>
+            <p>
+              Je suis convaincu que ma créativité et ma rigueur, enrichies par
+              ma passion pour la musique et le design, sont des atouts précieux
+              pour donner vie à vos projets web.
+            </p>
+            <p>
+              Confiez-moi votre projet web, et ensemble, tissons la toile du
+              succès{" "}
+            </p>
           </div>
           <div className="negatif"></div>
         </div>
